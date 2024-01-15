bca1376e2d32d411d410b566f80c5ef35354f4fa
cristianandrades <cristianam@ext.inditex.com>
2024-01-15 15:30:38 +0100

    Poc is now customizable.

--- app/src/main/java/es/wokis/poccolumn/MainActivity.kt
+++ app/src/main/java/es/wokis/poccolumn/MainActivity.kt
@@ -36,8 +36,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting(
-                        "Android",
-                        listOf(
+                        content = listOf(
                             "Lazy Column FTW 1",
                             "Lazy Column FTW 2",
                             "Lazy Column FTW 3",
@@ -54,7 +53,14 @@ class MainActivity : ComponentActivity() {
                             "Lazy Column FTW 14",
                             "Lazy Column FTW 15"
                         ),
-                        Modifier.padding(8.dp)
+                        staticElement = {
+                            StaticElement("name", Modifier)
+                        },
+                        dynamicElement = {
+                            Row {
+                                DynamicElement(it)
+                            }
+                        }
                     )
                 }
             }
@@ -63,43 +69,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, content: List<String>, modifier: Modifier = Modifier) {
+fun Greeting(
+    content: List<String>,
+    staticElement: @Composable () -> Unit,
+    dynamicElement: @Composable (element: String) -> Unit
+) {
     LazyColumn {
         item {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-            Text(
-                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum condimentum nisi, id suscipit leo sodales sed. In sit amet imperdiet sapien. Maecenas sit amet pretium libero. Quisque eleifend arcu felis, et pretium turpis pellentesque sed. Aenean in lorem non dui blandit ornare in in eros. Morbi sagittis mollis nunc, sit amet consequat mi sagittis sit amet. Maecenas lacinia eros ac ante commodo, quis consectetur eros imperdiet. Sed eleifend nunc dui, non tristique augue blandit a. Fusce aliquam accumsan rhoncus. Aliquam nec lacus dapibus, congue ligula quis, tincidunt enim. Ut vitae aliquam orci. Nulla efficitur ut dui interdum accumsan. Vestibulum turpis mauris, scelerisque eu viverra sit amet, mollis nec massa. Ut placerat venenatis tortor ac facilisis. Phasellus suscipit lobortis velit consectetur aliquam. Integer ullamcorper, diam ac lobortis placerat, sapien metus lobortis ex, at cursus enim nisl in dolor.",
-                modifier = modifier
-            )
-            Text(
-                text = "Curabitur scelerisque ac arcu ut malesuada. Aenean eget nulla sagittis, tempus nisl et, maximus ipsum. Vivamus vestibulum enim eu luctus dictum. Mauris nisi orci, interdum in suscipit sit amet, efficitur imperdiet erat. Pellentesque faucibus nisi ligula, in ornare urna vehicula sit amet. Aenean pellentesque eros enim, ut pharetra tortor efficitur ut. Cras eu dui ac sem pretium luctus in a ligula. Etiam consequat mi vel condimentum elementum. Vestibulum mattis molestie velit, nec posuere dolor gravida quis. Sed luctus eros at augue iaculis mollis. Maecenas ut dignissim nunc. Nam semper leo nec ante finibus, ut pharetra massa facilisis. Morbi non ipsum pretium justo sodales tincidunt vel ac nisl.",
-                modifier = modifier
-            )
-            Text(
-                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum condimentum nisi, id suscipit leo sodales sed. In sit amet imperdiet sapien. Maecenas sit amet pretium libero. Quisque eleifend arcu felis, et pretium turpis pellentesque sed. Aenean in lorem non dui blandit ornare in in eros. Morbi sagittis mollis nunc, sit amet consequat mi sagittis sit amet. Maecenas lacinia eros ac ante commodo, quis consectetur eros imperdiet. Sed eleifend nunc dui, non tristique augue blandit a. Fusce aliquam accumsan rhoncus. Aliquam nec lacus dapibus, congue ligula quis, tincidunt enim. Ut vitae aliquam orci. Nulla efficitur ut dui interdum accumsan. Vestibulum turpis mauris, scelerisque eu viverra sit amet, mollis nec massa. Ut placerat venenatis tortor ac facilisis. Phasellus suscipit lobortis velit consectetur aliquam. Integer ullamcorper, diam ac lobortis placerat, sapien metus lobortis ex, at cursus enim nisl in dolor.",
-                modifier = modifier
-            )
-            Text(
-                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum condimentum nisi, id suscipit leo sodales sed. In sit amet imperdiet sapien. Maecenas sit amet pretium libero. Quisque eleifend arcu felis, et pretium turpis pellentesque sed. Aenean in lorem non dui blandit ornare in in eros. Morbi sagittis mollis nunc, sit amet consequat mi sagittis sit amet. Maecenas lacinia eros ac ante commodo, quis consectetur eros imperdiet. Sed eleifend nunc dui, non tristique augue blandit a. Fusce aliquam accumsan rhoncus. Aliquam nec lacus dapibus, congue ligula quis, tincidunt enim. Ut vitae aliquam orci. Nulla efficitur ut dui interdum accumsan. Vestibulum turpis mauris, scelerisque eu viverra sit amet, mollis nec massa. Ut placerat venenatis tortor ac facilisis. Phasellus suscipit lobortis velit consectetur aliquam. Integer ullamcorper, diam ac lobortis placerat, sapien metus lobortis ex, at cursus enim nisl in dolor.",
-                modifier = modifier
-            )
+            staticElement()
         }
         items(content) {
-            Row {
-                Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "pepito")
-                Text(text = it, modifier = Modifier.background(Color.Black), color = Color.White)
-            }
+            dynamicElement(it)
         }
     }
+}
+
+@Composable
+private fun DynamicElement(it: String) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "pepito"
+    )
+    Text(text = it, modifier = Modifier.background(Color.Black), color = Color.White)
+}
+
+@Composable
+private fun StaticElement(name: String, modifier: Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Text(
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum condimentum nisi, id suscipit leo sodales sed. In sit amet imperdiet sapien. Maecenas sit amet pretium libero. Quisque eleifend arcu felis, et pretium turpis pellentesque sed. Aenean in lorem non dui blandit ornare in in eros. Morbi sagittis mollis nunc, sit amet consequat mi sagittis sit amet. Maecenas lacinia eros ac ante commodo, quis consectetur eros imperdiet. Sed eleifend nunc dui, non tristique augue blandit a. Fusce aliquam accumsan rhoncus. Aliquam nec lacus dapibus, congue ligula quis, tincidunt enim. Ut vitae aliquam orci. Nulla efficitur ut dui interdum accumsan. Vestibulum turpis mauris, scelerisque eu viverra sit amet, mollis nec massa. Ut placerat venenatis tortor ac facilisis. Phasellus suscipit lobortis velit consectetur aliquam. Integer ullamcorper, diam ac lobortis placerat, sapien metus lobortis ex, at cursus enim nisl in dolor.",
+        modifier = modifier
+    )
+    Text(
+        text = "Curabitur scelerisque ac arcu ut malesuada. Aenean eget nulla sagittis, tempus nisl et, maximus ipsum. Vivamus vestibulum enim eu luctus dictum. Mauris nisi orci, interdum in suscipit sit amet, efficitur imperdiet erat. Pellentesque faucibus nisi ligula, in ornare urna vehicula sit amet. Aenean pellentesque eros enim, ut pharetra tortor efficitur ut. Cras eu dui ac sem pretium luctus in a ligula. Etiam consequat mi vel condimentum elementum. Vestibulum mattis molestie velit, nec posuere dolor gravida quis. Sed luctus eros at augue iaculis mollis. Maecenas ut dignissim nunc. Nam semper leo nec ante finibus, ut pharetra massa facilisis. Morbi non ipsum pretium justo sodales tincidunt vel ac nisl.",
+        modifier = modifier
+    )
+    Text(
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum condimentum nisi, id suscipit leo sodales sed. In sit amet imperdiet sapien. Maecenas sit amet pretium libero. Quisque eleifend arcu felis, et pretium turpis pellentesque sed. Aenean in lorem non dui blandit ornare in in eros. Morbi sagittis mollis nunc, sit amet consequat mi sagittis sit amet. Maecenas lacinia eros ac ante commodo, quis consectetur eros imperdiet. Sed eleifend nunc dui, non tristique augue blandit a. Fusce aliquam accumsan rhoncus. Aliquam nec lacus dapibus, congue ligula quis, tincidunt enim. Ut vitae aliquam orci. Nulla efficitur ut dui interdum accumsan. Vestibulum turpis mauris, scelerisque eu viverra sit amet, mollis nec massa. Ut placerat venenatis tortor ac facilisis. Phasellus suscipit lobortis velit consectetur aliquam. Integer ullamcorper, diam ac lobortis placerat, sapien metus lobortis ex, at cursus enim nisl in dolor.",
+        modifier = modifier
+    )
+    Text(
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum condimentum nisi, id suscipit leo sodales sed. In sit amet imperdiet sapien. Maecenas sit amet pretium libero. Quisque eleifend arcu felis, et pretium turpis pellentesque sed. Aenean in lorem non dui blandit ornare in in eros. Morbi sagittis mollis nunc, sit amet consequat mi sagittis sit amet. Maecenas lacinia eros ac ante commodo, quis consectetur eros imperdiet. Sed eleifend nunc dui, non tristique augue blandit a. Fusce aliquam accumsan rhoncus. Aliquam nec lacus dapibus, congue ligula quis, tincidunt enim. Ut vitae aliquam orci. Nulla efficitur ut dui interdum accumsan. Vestibulum turpis mauris, scelerisque eu viverra sit amet, mollis nec massa. Ut placerat venenatis tortor ac facilisis. Phasellus suscipit lobortis velit consectetur aliquam. Integer ullamcorper, diam ac lobortis placerat, sapien metus lobortis ex, at cursus enim nisl in dolor.",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PocColumnTheme {
-        Greeting("Android", listOf("ejemplo", "aaaaaaaa"))
+        Greeting(listOf("ejemplo", "aaaaaaaa"), {}, {})
     }
 }
